cb408e03f03c831ff118f1ecccb233c1bdd7a978
Rodrigo A. Ribeiro <rodrigo_gillgamesh@hotmail.com>
2022-10-11 15:25:31 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require('./routes/auth')
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DBConection Successfull!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8080, () => {
+  console.log("Backend server is running!");
+});
